402c604739fa34f49c1ca29dbaba7746ee68fb93
Iam-Zarif <zarifraja19@gmail.com>
2025-05-22 00:45:11 +0600

    basic deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,3 @@
-import { generateMetadata } from "@/lib/metadata";
-
-export const metadata = generateMetadata({
-  title: "Home Page",
-  description: "Welcome to YouthBee",
-  keywords: ["Youthbee", "Web Development", "UI/UX"],
-  url: "https://yourdomain.com",
-  image: "https://i.ibb.co/rRStPmSp/youth-Bee-Logo.png",
-});
 
 export default function Home() {
   
